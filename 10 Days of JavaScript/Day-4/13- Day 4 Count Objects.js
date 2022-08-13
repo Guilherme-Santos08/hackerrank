3ec12dfc57cb1f397b94b890607fe8306217377a
Guilherme Ribeiro <augusto.gui.2208@gmail.com>
2022-08-13 17:51:02 -0300

    Day 4

---
+++ 10 Days of JavaScript/Day-4/13- Day 4 Count Objects.js
@@ -0,0 +1,19 @@
+/*
+ * Return a count of the total number of objects 'o' satisfying o.x == o.y.
+ *
+ * Parameter(s):
+ * objects: an array of objects with integer properties 'x' and 'y'
+ */
+function getCount(objects) {
+  return objects.filter(item => item.x === item.y).length
+}
+
+console.log(
+  getCount([
+    { x: 1, y: 1 },
+    { x: 2, y: 3 },
+    { x: 3, y: 3 },
+    { x: 3, y: 4 },
+    { x: 4, y: 5 },
+  ])
+)
